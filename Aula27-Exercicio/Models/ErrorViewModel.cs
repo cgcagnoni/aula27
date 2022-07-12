93af2f62c398aa82faa70601be5c24a6946d4afa
Caroliny S Q G Cagnoni <cgcagnoni@gmail.com>
2022-07-12 16:51:58 -0300

    Adicionar arquivos de projeto.

---
+++ Aula27-Exercicio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aula27_Exercicio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
